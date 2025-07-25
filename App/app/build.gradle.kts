75479c80de2ac6be6c131023e23dfae0a9281eb1
Minh Thanh <thanhhuynh0106@gmail.com>
2025-07-25 16:59:34 +0700

    update mainActivity

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.ui.graphics)
     implementation(libs.ui.tooling.preview)
     implementation(libs.material3)
+    implementation(libs.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
